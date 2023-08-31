89fb1b2711a3a0f962329ed698f574d786ecd064
EuGeni00 <eugeniotchanjongombe10@gmail.com>
2023-08-30 21:05:43 -0300

    manipulando o estado dos inputs

--- src/components/CampoDeTexto/index.js
+++ src/components/CampoDeTexto/index.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
-  console.log(props)
+  // console.log(props)
+  // let valor = "Eugenio";
+
+  const [valor, setValor] = useState("");
+
+  const aoDigitar = (evento) => {
+    setValor(evento.target.value);
+    console.log(valor);
+  }
+
   return (
     <div className="container" >
       {/* <h1>Digite os dados para o formulário</h1> */}
     <div className="campoTextoContent">
       <div className="campoTexto">
         <label id="name">{props.label}</label>
-        <input required={props.eObrigatorio} placeholder={props.placeholder}/>
+        <input 
+        value={valor}
+        onChange={aoDigitar} 
+        required={props.eObrigatorio} 
+        placeholder={props.placeholder}/>
       </div>
     </div>
     </div>
