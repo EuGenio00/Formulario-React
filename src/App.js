bac4d67089ab9ec7a0ea2a68c816b1cbd7cf5888
Eugenio00 <eugeniotchanjongombe10@gmail.com>
2023-09-13 20:19:28 -0300

    adicionando seção para times

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@
 import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
+
+  const times = [
+    {nome: 'Pedagógica',
+    corFundo: '#5b9bd5'
+    },
+    {nome: 'Administrativa',
+    corFundo: '#e7e6e6'
+    },
+    {nome: 'Instrutores',
+    corFundo: '#5b9bd5'
+    },
+    {nome: 'Coordenação',
+    corFundo: '#e7e6e6'
+    },
+    {nome: 'Discentes',
+    corFundo: '#5b9bd5'
+    }
+  ];
 
   const [colaboradores, setColaboradores] = useState([]);
 
@@ -17,6 +36,11 @@ function App() {
       <Banner />
       <Formulario 
       aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time tituloNome= "Pedagógica"/>
+      <Time tituloNome= "Coordenação"/>
+      <Time tituloNome= "Administrativa"/>
+      <Time tituloNome= "Instrutores"/>
+      <Time tituloNome= "Discentes"/>
     </div>
   );
 }
---
+++ src/components/Time/index.js
@@ -0,0 +1,11 @@
+import './Time.css'
+
+const Time = (props) => {
+  return(
+    <section className='time'>
+      <h3>{props.tituloNome}</h3>
+    </section>
+  )
+}
+
+export default Time;
